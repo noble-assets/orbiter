8c3a2f57577cb1432fe3225f6fe4b706d937b181
stepit <48993133+0xstepit@users.noreply.github.com>
2025-10-06 14:15:46 +0200

    chore: update error message
    Co-authored-by: MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>

--- keeper/component/forwarder/msg_server.go
+++ keeper/component/forwarder/msg_server.go
@@ -116,7 +116,7 @@ func (s msgServer) PauseCrossChains(
 
 	protocolID, err := core.NewProtocolIDFromString(msg.ProtocolId)
 	if err != nil {
-		return nil, errorsmod.Wrap(core.ErrUnableToUnpause, err.Error())
+		return nil, core.ErrUnableToUnpause.Wrapf("invalid protocol id: %s", err.Error())
 	}
 
 	if len(msg.CounterpartyIds) > core.MaxTargetCounterparties {
