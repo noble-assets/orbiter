af810ac36c905c81726a860e844ecbfb7f305c74
Malte Herrmann <malteherrmann.mail@gmail.com>
2025-10-15 15:57:07 +0200

    fix cleanup after generation of proto

--- proto/generate.sh
+++ proto/generate.sh
@@ -11,6 +11,6 @@ cp -r github.com/noble-assets/orbiter/* ./
 cp -r api/noble/orbiter/* api/
 find api/ -type f -name "*.go" -exec sed -i 's|github.com/noble-assets/orbiter/api/noble/orbiter|github.com/noble-assets/orbiter/api|g' {} +
 
-rm -rf github.com/noble-assets/orbiter
+rm -rf github.com
 rm -rf api/noble
 rm -rf noble
